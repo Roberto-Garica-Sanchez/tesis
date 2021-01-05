db5e77af3da4a3a7b76feefe836250db0f54ebad
Roberto <fullbetito@hotmail.com>
2021-01-05 11:01:04 -0600

    finalzado el pid + PWM 1 hilo

---
+++ 4 PWM/4 PWM .c
@@ -0,0 +1,61 @@
+
+#include <18F4550.h>
+#device ADC = 10
+#fuses NOMCLR INTRC_IO
+#use delay(clock = 8000000)
+#use fast_io(B)
+#use fast_io(C)
+#use fast_io(D)
+ 
+unsigned int16 i ;
+void main(){
+  setup_oscillator(OSC_8MHZ);            // Set internal oscillator to 8MHz
+  port_b_pullups(TRUE);                  // Enable PORTB pull-ups
+  output_b(0);                           // PORTB initial state
+  set_tris_b(7);                         // Configure RB0, RB1 & RB2 as inputs
+  output_c(0);                           // PORTC initial state
+  set_tris_c(0);                         // Configure PORTC pins as outputs
+  output_d(0);                           // PORTD initial state
+  set_tris_d(0);                         // Configure PORTD pins as outputs
+  setup_adc(ADC_CLOCK_DIV_8);            // Set ADC conversion time to 8Tosc
+  setup_adc_ports(AN0);                  // Configure AN0 as analog input
+  set_adc_channel(0);                    // Select channel AN0
+  setup_timer_2(T2_DIV_BY_16, 250, 1);   // Set PWM frequency to 500Hz
+  delay_ms(100);                         // Wait 100ms
+  while(TRUE){
+    i = read_adc();                      // leee AN0 y lo almacena en i 
+    set_pwm1_duty(i);                    // Set pwm1 duty cycle
+    delay_ms(10);                        // Wait 10ms
+    if(input(PIN_B0) == 0){              // If RB0 button pressed[isquierda] 
+      if(input(PIN_B3) == 0){            // If direction 1 not already selected[led]
+            output_b(0);                     // Both LEDs OFF
+            setup_ccp1(CCP_OFF);             // CCP1 OFF
+            output_c(0);                     // PORTC pins low
+            output_d(0);                     // PORTD pins low
+            delay_ms(100);                   // Wait 100ms
+            setup_ccp1(CCP_PWM | CCP_PWM_FULL_BRIDGE);
+                                            // Configure CCP1 as a PWM output forward
+            output_high(PIN_B3);             // RB3 LED ON
+        }
+    }
+    if(input(PIN_B1) == 0){              // If RB1 button pressed[derecha]
+      if(input(PIN_B4) == 0){            // If direction 2 not already selected[led]
+            output_b(0);                     // Both LEDs OFF
+            setup_ccp1(CCP_OFF);             // CCP1 OFF
+            output_c(0);                     // PORTC pins low
+            output_d(0);                     // PORTD pins low
+            delay_ms(100);                   // Wait 100ms
+            setup_ccp1(CCP_PWM | CCP_PWM_FULL_BRIDGE_REV);
+                                            // Configure CCP1 para multiplexado 
+            output_high(PIN_B4);             // RB4 LED ON
+        }
+    }
+    if(input(PIN_B2) == 0){              // If RB2 button pressed[STOP]
+      setup_ccp1(CCP_OFF);               // CCP1 OFF
+      output_b(0);                       // Both LEDs OFF
+      output_c(0);                       // PORTC pins low
+      output_d(0);                       // PORTD pins low
+      }
+   }
+}
+
--- PID/ccom/PID.c
+++ PID/ccom/PID.c
@@ -4,14 +4,33 @@
 #fuses XT,NOWDT,NOPROTECT,NOLVP,PUT,BROWNOUT,INTRC_IO  
 #use delay(clock=4000000)  
 
+//configuracion entradas/salidas
+//entradas
+    //[AN0]setpoint
+    //[AN1]posicion real 
+    //[]servo motor 
+//salidas
+    //control puenta H
+        //Grupos de rotacion 
+        //A
+            //[P1A][RC2]
+            //[P1D][RD7]
+        //B
+            //[P1C][RD6]
+            //[P1B][RD5]
+
+
+//#########     lecturas
+int setpoint;
+int posicion_real;
+//#########     PID
 int16 duty=0;
 int Timer2,Poscaler;
-//#########     PID
 float a,b,c;            //variables temporales
 float t=1;              //periodo de muestreo 
-float e;                //error
+float e[20];           //error
 float k=0;              //contrador de muestras
-float u[500];            //Salidas
+float u[20];            //Salidas
 //#########    ganancias
 float kd;               //Derivativa
 float ki;               //integral
@@ -28,24 +47,38 @@ void main() {
    setup_ccp1(ccp_pwm);                //Configurar modulo CCP1 en modo PWM
    setup_adc_ports(all_analog);        //Configurar ADC configuracion de los puertos que funcionan en analogico 
    setup_adc(adc_clock_internal);
-   while(1)
-   {
-        set_adc_channel(0);            //selecciona el puesto en el que se ejecutarn las sigientes instruciones 
-        delay_us(100);       
+   while(1){
+       
+        //muestreo
+            //lectura del set point
+                set_adc_channel(0);            //selecciona el puesto en el que se ejecutarn las sigientes instruciones 
+                delay_us(100);       
+                setpoint=read_adc();
+            //lectura del set point
+                set_adc_channel(1);            //selecciona el puesto en el que se ejecutarn las sigientes instruciones 
+                delay_us(100);       
+                posicion_real=read_adc();
         //duty=read_adc();               //leer
         //muestreo
-            e[k]=read_adc();
+            e[k]=posicion_real-setpoint;
+            
         //calculo
             a=e[k]  *( kp + (ki*t) )+( kd/t );
             b=e[k-1]*( (-2*(kd/t) - kp) );
             c=e[k-2]*( (kd/t) +u[k-1] );
             u[k]=a+b+c;
         //incremento en contrador 
-            k++;
+            k=k+1;
                     
         //salidas 
-            set_pwm1_duty(duty);           //write salida
+            set_pwm1_duty( u[k]);           //write salida
         //protecion contra desvordamiento de variables 
-        if(k>=500)break;
+        if(k>=20){
+            k=0; 
+            for (int i = 0; i <20; i++){
+                e[i]=0;
+            }
+            
+        }
    }
 }
